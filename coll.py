2b85a550173160e4238f9809a6b19da5889eb959
Magnus Breder Birkenes <12692786+magbb@users.noreply.github.com>
2024-01-22 14:03:12 +0100

    Update coll.py

--- coll.py
+++ coll.py
@@ -177,7 +177,7 @@ def print_concordances(conc, query):
         urn = row[1]["urn"]
 
         if urn.startswith('URN:'):
-            metadata = corpus[corpus["urn"] == urn][['title', 'authors', 'year', 'timestamp']]
+            metadata = corpus[corpus["urn"] == urn]
             metadata = metadata.iloc[0]
 
             if 'digavis' in urn:
@@ -193,7 +193,7 @@ def print_concordances(conc, query):
             url = "https://www.nb.no/items/%s?searchText=%s" % (urn, query)
             link = "<a href='%s' target='_blank'>%s – %s – %s</a>" % (url, metadata["title"], metadata["authors"], timestamp)
         else:
-            metadata = corpus[corpus["urn"] == urn][['city', 'language', 'timestamp']]
+            metadata = corpus[corpus["urn"] == urn]
             metadata = metadata.iloc[0]
             timestamp = metadata["timestamp"]
             url = urn
