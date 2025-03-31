71bc9dc238c1aefe27e34806490bda5e6a347f2d
iciclejj <55782191+iciclejj@users.noreply.github.com>
2025-03-31 14:26:42 +0200

    Equalize column top positions

--- coll.py
+++ coll.py
@@ -255,22 +255,23 @@ colls["Relevans"] = colls["Relevans"].round(2)
 excel_colls = to_excel(colls)
 excel_corpus = to_excel(corpus)
 
-col1, col2 = st.columns(2)
+col1_1, col1_2 = st.columns(2) # row 1
 
 if st.session_state.corpus_upload is None:
-    with col1:
+    with col1_1:
         st.markdown("__Korpusstørrelse:__ " + str(len(corpus)) + " dokumenter. " + "Eksporter " + get_table_download_link(excel_corpus, link_content="korpusdefinisjon.", filename="corpus.xlsx"), unsafe_allow_html=True)
 else:
-    with col1:
+    with col1_1:
         st.markdown("__Korpusstørrelse:__ " + str(len(corpus)) + " dokumenter (__opplastet korpusdefinsjon__). ",  unsafe_allow_html=True)
 
-with col2:
+with col1_2:
     st.markdown("Eksporter " + get_table_download_link(excel_colls, link_content="kollokasjonstabell", filename="collocations.xlsx") + ".", unsafe_allow_html=True)
 
+col2_1, col2_2 = st.columns(2) # row 2
 
-with col1:
+with col2_1:
     selection = st.dataframe(colls, hide_index=True, on_select="rerun", selection_mode="single-row", use_container_width=True)
-with col2:
+with col2_2:
     try:
         wc = get_wordcloud(colls[["Kollokat", sort_by]].set_index("Kollokat"), top=head)
         wc_svg = wc.to_svg()
